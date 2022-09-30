61b1e7ff092c340cdd17dfc9275e720a431f9b06
Mark Kolenbrander <93470870+Canto86@users.noreply.github.com>
2022-09-30 15:56:50 +0200

    Commit 1 - HW 7
    - Added Worker Dependency
    
    - Commit because of change computer

--- app/src/main/java/com/markkolenbrander/capstonenewsapp/DetailFragment.kt
+++ app/src/main/java/com/markkolenbrander/capstonenewsapp/DetailFragment.kt
@@ -1,18 +1,29 @@
 package com.markkolenbrander.capstonenewsapp
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
+import androidx.work.*
 import com.bumptech.glide.Glide
 import com.markkolenbrander.capstonenewsapp.databinding.FragmentDetailBinding
+import com.markkolenbrander.capstonenewsapp.worker.DownloadImageWorker
+import com.markkolenbrander.capstonenewsapp.worker.DownloadWorker
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
 
 class DetailFragment : Fragment() {
 
     private lateinit var binding: FragmentDetailBinding
     private val args by navArgs<DetailFragmentArgs>()
+    private val item = args.article.urlToImage
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,9 +45,50 @@ class DetailFragment : Fragment() {
         binding.tvArticleContent.text = args.article.content
         binding.tvArticleTitle.text = args.article.title
 
-        val imgView = binding.ivImgUrl
-        val item = args.article.urlToImage
-        context?.let { Glide.with(it).load(item).into(imgView) }
+        //Todo: Check here to go further
+        args.article.urlToImage?.let { onImageDownload(it) }
 
+//        val imgView = binding.ivImgUrl
+//        val item = args.article.urlToImage
+//        context?.let { Glide.with(it).load(item).into(imgView) }
+    }
+
+
+    private fun onImageDownload(imageUrl: String) {
+        val constraints = Constraints.Builder()
+            .setRequiresBatteryNotLow(true)
+            .setRequiresStorageNotLow(true)
+            .setRequiredNetworkType(NetworkType.NOT_ROAMING)
+            .build()
+
+        val downloadImageWorker = OneTimeWorkRequestBuilder<DownloadImageWorker>()
+            .setInputData(workDataOf("image_path" to imageUrl))
+            .setConstraints(constraints)
+            .build()
+
+        val workManager = context?.let { WorkManager.getInstance(it) }
+        workManager?.enqueue(downloadImageWorker)
+
+        workManager?.getWorkInfoByIdLiveData(downloadImageWorker.id)
+            ?.observe(viewLifecycleOwner, Observer { info ->
+                if (info.state.isFinished) {
+                    val imageFile =
+                        activity?.let { Glide.with(it).asFile().load(item).submit().get() }
+                    if (imageFile != null) {
+                        displayImage(imageFile.path)
+                    }
+                }
+            })
+    }
+
+    private fun displayImage(imagePath: String) {
+        GlobalScope.launch(Dispatchers.Main) {
+            val bitmap = loadImageFromFile(imagePath)
+            binding.ivImgUrl.setImageBitmap(bitmap)
+        }
+    }
+
+    private suspend fun loadImageFromFile(imagePath: String) = withContext(Dispatchers.IO) {
+        BitmapFactory.decodeFile(imagePath)
     }
 }
---
+++ app/src/main/java/com/markkolenbrander/capstonenewsapp/worker/DownloadWorker.kt
@@ -0,0 +1,40 @@
+package com.markkolenbrander.capstonenewsapp.worker
+
+import android.content.Context
+
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import java.io.File
+import java.io.FileOutputStream
+import java.net.HttpURLConnection
+import java.net.URL
+
+class DownloadWorker(context: Context, workerParameters: WorkerParameters)
+    : Worker(context, workerParameters) {
+
+    override fun doWork(): Result {
+        val imageURL = URL("url")
+        val connection = imageURL.openConnection() as HttpURLConnection
+        connection.doInput = true
+        connection.connect()
+
+        val imagePath = "news_image.jpg"
+        val inputStream = connection.inputStream
+        val file = File(applicationContext.externalMediaDirs.first(), imagePath)
+
+        val outputStream = FileOutputStream(file)
+        outputStream.use { output ->
+            val buffer = ByteArray(4 * 1024)
+
+            var byteCount = inputStream.read(buffer)
+
+            while (byteCount > 0) {
+                output.write(buffer, 0 , byteCount)
+
+                byteCount = inputStream.read(buffer)
+            }
+            outputStream.flush()
+        }
+        return Result.success()
+    }
+}
